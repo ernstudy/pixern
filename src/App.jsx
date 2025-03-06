eb37d755b72c6c1339c975c1ed211870afdf63ba
ernstudy <hey@ernstudy.com>
2025-03-06 15:36:05 -0500

    improve code organization

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import "./App.css";
 // components
 import Cover from "./components/Cover";
 import ImageDisplay from "./components/ImageDisplay";
+import CopyRigthComponent from "./components/CopyRigthComponent";
 
 export default function App() {
   return (
     <>
       <Cover />
       <ImageDisplay />
+
+      <CopyRigthComponent />
     </>
   );
 }
