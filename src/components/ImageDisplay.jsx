4e8d3ee9a65d859293cdd9da872d45b27fa7071b
ernstudy <hey@ernstudy.com>
2025-03-06 15:37:53 -0500

    add disply image component

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import PrevNextNavigator from "./PrevNextNavigator";
 
 export default function ImageDisplay() {
   return (
-    <div className="main">
+    <div className="image-display">
       <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
 
       <div className="images-container">
-        <div className="image"></div>
+        <div className="images"></div>
       </div>
+
+      <PrevNextNavigator />
     </div>
   );
 }
