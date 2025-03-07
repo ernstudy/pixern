5581e2e0d1a57229266089fd23d309d60297eb60
ernstudy <hey@ernstudy.com>
2025-03-07 15:58:36 -0500

    Add input functionallity

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,29 @@
 import React, { useContext } from "react";
 import { InputContext } from "../context/InputContext";
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
   const { inputValue, setInputValue } = useContext(InputContext);
-  console.log(inputValue);
+
+  const hundleInput = () => {
+    onSearch(inputValue);
+  };
+
   return (
     <div className="search-bar">
-      <box-icon name="search-alt-2" className="icon"></box-icon>
+      <box-icon
+        name="search-alt-2"
+        className="icon"
+        onClick={hundleInput}
+      ></box-icon>
       <input
         type="text"
         name=""
         placeholder="Search for free images..."
         onChange={(e) => setInputValue(e.target.value)}
         value={inputValue}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") return hundleInput(inputValue);
+        }}
       />
     </div>
   );
