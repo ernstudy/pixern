3ec61cceefe153efcfcd2646573ea7a6f0cb025d
ernstudy <hey@ernstudy.com>
2025-03-11 07:29:50 -0400

    add button pages to global state of input context

--- src/context/InputContext.jsx
+++ src/context/InputContext.jsx
@@ -6,12 +6,20 @@ export const InputProvider = ({ children }) => {
   // use state
   const [inputValue, setInputValue] = useState("");
   const [numOfpages, setNumOfPages] = useState(1);
+  const [buttonPages, setButtonPages] = useState(5);
   // use memo
   // const value = useMemo(() => ({ inputValue, setInputValue }), [inputValue]);
 
   return (
     <InputContext.Provider
-      value={{ inputValue, setInputValue, numOfpages, setNumOfPages }}
+      value={{
+        inputValue,
+        setInputValue,
+        numOfpages,
+        setNumOfPages,
+        buttonPages,
+        setButtonPages,
+      }}
     >
       {children}
     </InputContext.Provider>
