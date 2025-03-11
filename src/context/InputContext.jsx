51efacbbd7191b651954355d06135ab6245a3824
ernstudy <hey@ernstudy.com>
2025-03-11 05:19:11 -0400

    create numOfPages global state

--- src/context/InputContext.jsx
+++ src/context/InputContext.jsx
@@ -5,10 +5,15 @@ export const InputContext = createContext();
 export const InputProvider = ({ children }) => {
   // use state
   const [inputValue, setInputValue] = useState("");
+  const [numOfpages, setNumOfPages] = useState(1);
   // use memo
-  const value = useMemo(() => ({ inputValue, setInputValue }), [inputValue]);
+  // const value = useMemo(() => ({ inputValue, setInputValue }), [inputValue]);
 
   return (
-    <InputContext.Provider value={value}>{children}</InputContext.Provider>
+    <InputContext.Provider
+      value={{ inputValue, setInputValue, numOfpages, setNumOfPages }}
+    >
+      {children}
+    </InputContext.Provider>
   );
 };
