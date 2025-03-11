2ca67ba7587cdc968d99ef48e6ce72e754fbf591
ernstudy <hey@ernstudy.com>
2025-03-11 07:28:05 -0400

    change buttons pages apparence

--- src/components/PrevNextNavigator.jsx
+++ src/components/PrevNextNavigator.jsx
@@ -8,46 +8,69 @@ import { Button, styled } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
 import { InputContext } from "../context/InputContext";
 
-const StyledButton = styled(Button)(`background: #ffc524;
-    font-size: 14px;
+export default function PrevNextNavigator() {
+  const { numOfpages, setNumOfPages, buttonPages, setButtonPages } =
+    useContext(InputContext);
+
+  const StyledButton = styled(Button)(`background: #ffc524;
     text-transform: none;
     color: #161618;
     margin: 10px;
+    padding: 0;
+    width: max-content;
     `);
+  // Define the base style for the buttons
+  const ButtonStyle = {
+    fontSize: "30px",
+    color: "#161618",
+    backgroundColor: "#ffc524",
+    padding: "0",
+    width: "30px",
+    height: "25px",
+    margin: "0 16px",
+    cursor: "pointer",
+    fontWeight: "bolder",
+  };
 
-export default function PrevNextNavigator() {
-  const { numOfpages, setNumOfPages } = useContext(InputContext);
-  const [pages, setPages] = useState(5);
+  // Apply ButtonStyle to multiple components
+  const StyledNavigateNext = styled(NavigateNext)(ButtonStyle);
 
+  const StyledNavigateBefore = styled(NavigateBefore)(ButtonStyle);
+
+  // next and preview buttons fuctionality
   const hundleClick = (num) => {
     setNumOfPages(numOfpages + num);
     console.log("number of page", numOfpages);
-    setPages(pages + num);
+
+    if (numOfpages >= 5) {
+      setButtonPages(buttonPages + 1);
+    }
   };
 
   const hundlePages = (e) => {
     const number = parseInt(e.target.textContent);
     setNumOfPages(number);
 
-    if (number >= pages) {
-      setPages(number + 1);
+    if (number >= buttonPages) {
+      setButtonPages(number + 1);
     }
 
-    console.log("pages", pages);
+    console.log("pages", buttonPages);
   };
 
   return (
     <div className="prev_next-navigator">
-      <StyledButton
-        variant="contained"
-        disabled={false}
-        endIcon={<NavigateNext />}
-        onClick={() => hundleClick(+1)}
-      >
-        Next
-      </StyledButton>
+      <StyledNavigateBefore
+        onClick={() => hundleClick(-1)}
+        sx={
+          numOfpages == 1 && {
+            visibility: "collapse",
+          }
+        }
+      />
+
       <div className="pages">
-        {Array.from({ length: pages }).map((_, index) => (
+        {Array.from({ length: buttonPages }).map((_, index) => (
           <div
             key={index}
             className={index + 1 == numOfpages ? "page active" : "page"}
@@ -57,14 +80,11 @@ export default function PrevNextNavigator() {
           </div>
         ))}
       </div>
-      <StyledButton
-        variant="contained"
-        disabled={numOfpages <= 1 && true}
-        startIcon={<NavigateBefore />}
-        onClick={() => hundleClick(-1)}
-      >
-        Prev
-      </StyledButton>
+
+      <StyledNavigateNext
+        className="styled-button"
+        onClick={() => hundleClick(+1)}
+      />
     </div>
   );
 }
