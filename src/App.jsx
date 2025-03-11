74b5c101df4320d01f64575f90fa0e4fec2aca1f
ernstudy <hey@ernstudy.com>
2025-03-11 05:14:24 -0400

    improuve code organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 // css
 import "./App.css";
@@ -7,17 +7,47 @@ import "./App.css";
 import Cover from "./components/Cover";
 import ImageDisplay from "./components/ImageDisplay";
 import CopyRigthComponent from "./components/CopyRigthComponent";
+import { InputContext } from "./context/InputContext";
 
 export default function App() {
-  const searchImage = (inputText) => {
-    console.log("loarding...", inputText);
-    // console.log("loarding...2", text);
+  const per_page = 10;
+  const [imageData, setImageData] = useState(null);
+  const { numOfpages } = useContext(InputContext);
+
+  const API_URL = import.meta.env.VITE_API_URL;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    searchImage();
+  }, [numOfpages]);
+
+  useEffect(() => {
+    searchImage();
+  }, []);
+
+  const searchImage = async (inputText = "random") => {
+    console.log(API_KEY);
+    console.log(API_URL);
+
+    try {
+      const data = await fetch(
+        `${API_URL}?query=${inputText}&page=${numOfpages}&per_page=${per_page}&client_id=${API_KEY}`
+      );
+      if (!data.ok)
+        throw new Error(`Error: ${data.status} - ${data.statusText}`);
+
+      const response = await data.json();
+      console.log(response.results);
+      setImageData(response.results);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <>
       <Cover onSearch={searchImage} />
-      <ImageDisplay />
+      <ImageDisplay imageData={imageData} />
 
       <CopyRigthComponent />
     </>
