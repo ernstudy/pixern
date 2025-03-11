d3f068b09a9a4bd30503d1c43b3445dc04bb8dd7
ernstudy <hey@ernstudy.com>
2025-03-11 05:19:58 -0400

    display images

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import PrevNextNavigator from "./PrevNextNavigator";
 
-export default function ImageDisplay() {
+export default function ImageDisplay({ imageData }) {
+  console.log(imageData);
+
+  // const { image, setImage } = useState({});
+
+  // const getImageItems = () => {
+  //   imageData?.forEach((item) => {
+  //     console.log(item.urls.full);
+  //     const imageItem = {
+  //       image: item.urls.full,
+  //     };
+  //     return imageItem;
+  //   });
+
+  //   setImage(imageItem);
+  //   console.log(imageItem);
+  // };
+
+  // getImageItems();
+
   return (
     <div className="image-display">
       <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
 
       <div className="images-container">
-        <div className="images"></div>
+        {imageData &&
+          imageData.map((image) => (
+            <div className="image" width={image.width} key={image.id}>
+              <img src={image.urls.full} alt="" />
+            </div>
+          ))}
       </div>
 
       <PrevNextNavigator />
