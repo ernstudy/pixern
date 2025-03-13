31073b4f618266414403573c258b85c33578a86b
ernstudy <hey@ernstudy.com>
2025-03-13 13:21:54 -0400

    add functionallity to dwonload button in GalleryImageModal component and set it loadiong transition

--- src/components/GalleryImageModal.jsx
+++ src/components/GalleryImageModal.jsx
@@ -2,12 +2,42 @@ import { Close, Download } from "@mui/icons-material";
 import { Button, styled } from "@mui/material";
 import React from "react";
 
-export default function GalleryImageModal({ imageDetails, activeModal }) {
+export default function GalleryImageModal({
+  imageDetails,
+  activeModal,
+  setActiveModal,
+  handleDownload,
+  downloading,
+  isID,
+}) {
   // show modal - image gallery
 
-  const StyledDownloadButton = styled(Button)({ backgroundColor: "blue" });
+  const StyledDownloadButton = styled(Button)({
+    backgroundColor: "#ffc524",
+    color: "#161618",
+    fontSize: "16px",
+    textTransform: "inherit",
+    transition: "all 0.5s",
 
-  const StyledCloseButton = styled(Close)({});
+    "&:hover": {
+      backgroundColor: "#e9ab00",
+    },
+  });
+
+  const StyledCloseButton = styled(Close)({
+    background: "#7c7c83",
+    width: "30px",
+    height: "30px",
+    padding: "3px",
+    fontSize: "20px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    transition: "all 0.5s",
+
+    "&:hover": {
+      backgroundColor: "#69696f",
+    },
+  });
 
   // image data
   const imageData = {
@@ -16,23 +46,34 @@ export default function GalleryImageModal({ imageDetails, activeModal }) {
     title: imageDetails?.alt_description,
   };
 
+  const closeModal = () => {
+    setActiveModal(!activeModal);
+    document.body.style = "overflow: auto";
+  };
   return (
     <>
-      {imageDetails && (
+      {imageData.image && (
         <div className={activeModal ? "gallery-modal active" : "gallery-modal"}>
           <div className="modal-box">
-            <div className="modal-image">
-              <img src={imageData.image} alt={imageData.title} />
-            </div>
-            <div className="modal-image-title">
-              <h2>{imageData.title}</h2>
+            <div className="image-and-title">
+              <div className="modal-image-title">
+                <h2>{imageData.title}</h2>
+              </div>
+              <div className="modal-image">
+                <img src={imageData.image} alt={imageData.title} />
+              </div>
             </div>
             <div className="modal-image-buttons">
-              <StyledDownloadButton endIcon={<Download />}>
+              <StyledDownloadButton
+                endIcon={<Download />}
+                onClick={() => handleDownload(imageDetails)}
+                loading={imageData.id == isID && downloading}
+                loadingPosition="start"
+              >
                 Download
               </StyledDownloadButton>
 
-              <StyledCloseButton />
+              <StyledCloseButton onClick={closeModal} />
             </div>
           </div>
         </div>
