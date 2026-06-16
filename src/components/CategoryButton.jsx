11ec505dfe4b127672245139a48528357fd0abcc
ernstudy <saingillese@gmail.com>
2026-06-16 15:56:07 -0400

    refactor CategoryButton component to remove unused onSearch prop and include setNumOfPages for improved state management

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -2,16 +2,17 @@ import { useContext } from "react";
 import { ImagesContext } from "../context/ImagesContext";
 import { categories } from "../utils/category.utils";
 
-export default function CategoryButton({ onSearch }) {
-  const { setQuery, setButtonPages } = useContext(ImagesContext);
+export default function CategoryButton() {
+  const { setQuery, setButtonPages, setNumOfPages } = useContext(ImagesContext);
 
   const hundleCategories = (e) => {
     const category = e.target.textContent;
     setQuery(category);
-    onSearch(category);
 
     // reset button pages to 5 and number of pages to 1
     setButtonPages(5);
+
+    setNumOfPages(1);
   };
 
   return (
