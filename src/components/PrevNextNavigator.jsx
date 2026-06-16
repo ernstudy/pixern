e04095aea09a31fec9d694c646304dd52cb7ff7c
ernstudy <saingillese@gmail.com>
2026-06-16 01:10:25 -0400

    refactor PrevNextNavigator component to use ImagesContext and simplify button styling

--- src/components/PrevNextNavigator.jsx
+++ src/components/PrevNextNavigator.jsx
@@ -1,61 +1,29 @@
 import {
-  NavigateBefore,
-  NavigateNext,
-  Preview,
-  Tune,
-} from "@mui/icons-material";
-import { Button, styled } from "@mui/material";
-import React, { useContext, useEffect, useState } from "react";
-import { InputContext } from "../context/InputContext";
+  StyledNavigateBefore,
+  StyledNavigateNext,
+} from "../utils/styles/navigationButtonsStyles";
+import { useContext } from "react";
+import { ImagesContext } from "../context/ImagesContext";
 
 export default function PrevNextNavigator() {
-  const { numOfpages, setNumOfPages, buttonPages, setButtonPages } =
-    useContext(InputContext);
+  const { setNumOfPages, numOfpages } = useContext(ImagesContext);
 
-  const StyledButton = styled(Button)(`background: #ffc524;
-    text-transform: none;
-    color: #161618;
-    margin: 10px;
-    padding: 0;
-    width: max-content;
-    `);
-  // Define the base style for the buttons
-  const ButtonStyle = {
-    fontSize: "30px",
-    color: "#161618",
-    backgroundColor: "#ffc524",
-    padding: "0",
-    width: "30px",
-    height: "25px",
-    margin: "0 16px",
-    cursor: "pointer",
-    fontWeight: "bolder",
+  const hundlePages = (e) => {
+    const num = parseInt(e.target.textContent);
+    setNumOfPages(num);
+
+    if (num >= buttonPages) {
+      setButtonPages(number + 1);
+    }
   };
-
-  // Apply ButtonStyle to multiple components
-  const StyledNavigateNext = styled(NavigateNext)(ButtonStyle);
-
-  const StyledNavigateBefore = styled(NavigateBefore)(ButtonStyle);
 
   // next and preview buttons fuctionality
   const hundleClick = (num) => {
     setNumOfPages(numOfpages + num);
-    console.log("number of page", numOfpages);
 
     if (numOfpages >= 5) {
       setButtonPages(buttonPages + 1);
     }
-  };
-
-  const hundlePages = (e) => {
-    const number = parseInt(e.target.textContent);
-    setNumOfPages(number);
-
-    if (number >= buttonPages) {
-      setButtonPages(number + 1);
-    }
-
-    console.log("pages", buttonPages);
   };
 
   return (
