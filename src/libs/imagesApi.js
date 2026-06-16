468ccd7ea6c390a5542809d4a2a6ed648f556d85
ernstudy <saingillese@gmail.com>
2026-06-16 16:00:04 -0400

    refactor fetchImages function to accept numOfpages as a parameter for improved flexibility

--- src/libs/imagesApi.js
+++ src/libs/imagesApi.js
@@ -4,9 +4,8 @@ const baseUrlApi = envConfig.baseApiUrl;
 const apiKey = envConfig.apiKey;
 
 const per_page = 10;
-const numOfpages = 1;
 
-const fetchImages = async (query = "random") => {
+const fetchImages = async (query, numOfpages) => {
   const data = await fetch(
     `${baseUrlApi}/search/photos?query=${query}&page=${numOfpages}&per_page=${per_page}&client_id=${apiKey}`,
   );
