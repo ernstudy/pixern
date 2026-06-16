7fdb6dbd805f9aed5ef4cb6613c760e20c718b52
ernstudy <saingillese@gmail.com>
2026-06-16 01:25:16 -0400

    refactor ImageDisplay component to use envConfig for API key and simplify download button styling

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -3,43 +3,32 @@ import PrevNextNavigator from "./PrevNextNavigator";
 import { Close, Download } from "@mui/icons-material";
 import { Button, colors, styled } from "@mui/material";
 import GalleryImageModal from "./GalleryImageModal";
+import { StyledDownloadButton } from "../utils/styles/StyledDownloadButton";
+import { envConfig } from "../config/env.config";
 
 export default function ImageDisplay({ imageData }) {
-  // download button style
-  const StyledDownloadButton = styled(Button)({
-    background: "#ffc524",
-    borderRadius: "5px",
-    color: "#434347",
-    "&:hover": {
-      color: "#ffc524",
-      background: "#434347",
-      transition: "all 0.5s",
-    },
-  });
-
   // ===== download image  =======
-
   const [downloading, setDownloading] = useState(false);
   const [isID, setIsID] = useState(null);
+  const [activeModal, setActiveModal] = useState(false);
+  const [imageDetails, setImageDetails] = useState();
+
   const handleDownload = async (image) => {
-    const accessKey = import.meta.env.VITE_API_KEY;
+    const accessKey = envConfig.apiKey;
     const photoId = image.id;
     console.log("unique image ID:", photoId);
     try {
-      // Solicitar la imagen como blob
       const response = await fetch(
         `https://api.unsplash.com/photos/${photoId}/download?w=400&q=60`,
         {
           headers: {
             Authorization: `Client-ID ${accessKey}`,
           },
-        }
+        },
       );
       // downloading
       setIsID(photoId);
       setDownloading(true);
-
-      console.log("response", response);
 
       if (!response.ok) {
         setDownloading(false);
@@ -47,8 +36,6 @@ export default function ImageDisplay({ imageData }) {
       }
 
       const downloadData = await response.json();
-
-      console.log("download", downloadData);
 
       // Obtener la imagen desde la URL de descarga
       const imageResponse = await fetch(downloadData.url);
@@ -59,12 +46,10 @@ export default function ImageDisplay({ imageData }) {
         setDownloading(false);
       }
 
-      console.log("blod", blob);
-
       // Crear un enlace temporal para descargar la imagen
       const link = document.createElement("a");
       link.href = URL.createObjectURL(blob);
-      link.download = `${image.alt_description || "image"}-PixErn.jpg`; // Nombre del archivo
+      link.download = `${image.alt_description || "image"}-PixErn.jpg`;
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
@@ -74,15 +59,12 @@ export default function ImageDisplay({ imageData }) {
 
       // downloading
     } catch (error) {
-      console.error("Ocurrió un error:", error.message);
+      console.error("Error:", error.message);
       setDownloading(false);
     }
   };
 
   // show image modal
-
-  const [activeModal, setActiveModal] = useState(false);
-  const [imageDetails, setImageDetails] = useState();
   const showModal = (image) => {
     console.log("clicked on this image => :", image);
     setImageDetails(image);
@@ -92,8 +74,6 @@ export default function ImageDisplay({ imageData }) {
 
   return (
     <div className="image-display">
-      {/* <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2> */}
-
       <div className="images-container">
         {imageData &&
           imageData.map((image) => (
